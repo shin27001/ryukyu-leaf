9afbd5cf3a90d44608c224c3c0859a8dbefe8762
akatukik <akatukik@gmail.com>
2020-06-30 14:29:44 +0900

    no message

--- header-sub.php
+++ header-sub.php
@@ -1,8 +1,4 @@
   <div class="l-mainvisual">
-    <p><img src="<?php echo get_template_directory_uri(); ?>/images/home-logo.png" alt="<?php bloginfo('name'); ?>"></p>
-  </div>
-
-  <div class="l-mainvisual">
     <div class="l-mainvisual__img">
       <img src="<?php echo get_template_directory_uri(); ?>/images/slide/slide-02.jpg" alt="<?php bloginfo('name'); ?>">
     </div>
