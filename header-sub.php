bf5afde3f76ae684d5f499ddaf17fdac15fa1895
akatukik <akatukik@gmail.com>
2020-06-30 15:23:37 +0900

    画像変更

--- header-sub.php
+++ header-sub.php
@@ -1,6 +1,6 @@
   <div class="l-mainvisual">
     <div class="l-mainvisual__img">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/slide/slide-02.jpg" alt="<?php bloginfo('name'); ?>">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/slide/slide-01.jpg" alt="<?php bloginfo('name'); ?>">
     </div>
     <p class="l-mainvisual__title">
       <img src="<?php echo get_template_directory_uri(); ?>/images/hd-logo.png" alt="<?php bloginfo('name'); ?>">
