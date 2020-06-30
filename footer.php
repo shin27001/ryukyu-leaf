a4cba464d0726c462e14a501f6d39ddd7102c1bf
akatukik <akatukik@gmail.com>
2020-06-30 13:19:09 +0900

    jsのリンクを追加

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/scripts/vendors/slick.js"></script>
       <script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js?<?php echo date('Ymd-Hi'); ?>"></script>
+      <script src="<?php echo get_template_directory_uri(); ?>scripts/libs/main-slider.js"></script>
       </body>
 
       </html>
