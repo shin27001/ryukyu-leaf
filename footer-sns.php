a7ec87d385c28cab743036c594d1411bba68bffe
akatukik <akatukik@gmail.com>
2020-07-10 12:27:59 +0900

    SNSシェアボタン 設定（途中まで） CSS修正

--- footer-sns.php
+++ footer-sns.php
@@ -1,10 +1,15 @@
 <div class="mod_share">
   <h3 class="mod_share__title">この情報を共有する</h3>
   <ul class="mod_share__list">
-    <li class="mod_share__item twitter"><a href="#" class="link" target="_blank">
+    <li class="mod_share__item twitter"><a href="https://twitter.com/share?
+  url=<?php echo get_the_permalink(); ?>&
+  via={Twitter ID}&
+  related={Twitter ID}&
+  hashtags=web,webdesign&
+  text=<?php echo get_the_title(); ?>" rel="nofollow" target="_blank">
         <p class="txt"><i class="fab fa-twitter symbol"></i><br>Twitterで<br>シェア</p>
       </a></li>
-    <li class="mod_share__item facebook"><a href="#" class="link" target="_blank">
+    <li class="mod_share__item facebook"><a href="http://www.facebook.com/share.php?u={URL}" class="link" target="_blank">
         <p class="txt"><i class="fab fa-facebook symbol"></i><br>Facebookで<br>シェア</p>
       </a></li>
     <li class="mod_share__item line"><a href="#" class="link">
