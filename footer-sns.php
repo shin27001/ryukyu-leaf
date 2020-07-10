de7ec2dfb4b54c27f8f2d7f832b4b520d46bcc6e
akatukik <akatukik@gmail.com>
2020-07-10 14:44:21 +0900

    SNS設定修正

--- footer-sns.php
+++ footer-sns.php
@@ -1,12 +1,7 @@
 <div class="mod_share">
   <h3 class="mod_share__title">この情報を共有する</h3>
   <ul class="mod_share__list">
-    <li class="mod_share__item twitter"><a href="https://twitter.com/share?
-  url=<?php echo get_the_permalink(); ?>&
-  via={Twitter ID}&
-  related={Twitter ID}&
-  hashtags=web,webdesign&
-  text=<?php echo get_the_title(); ?>" rel="nofollow" target="_blank">
+    <li class="mod_share__item twitter"><a href="#>" target="_blank">
         <p class="txt"><i class="fab fa-twitter symbol"></i><br>Twitterで<br>シェア</p>
       </a></li>
     <li class="mod_share__item facebook"><a href="http://www.facebook.com/share.php?u={URL}" class="link" target="_blank">
