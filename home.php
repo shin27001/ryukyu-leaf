cd90bf73811b28ae1a0274215aa137ff794e1fd5
matayoshi <matayoshi@sem-cloud.com>
2020-08-19 13:49:29 +0900

    TOPページ、インスタ「ごはん旅の投稿を見る」ボタンの表示崩れ修正

--- home.php
+++ home.php
@@ -92,8 +92,10 @@
         応援したいお店をシェアして広めよう！<br>
         <span>#<?php get_pref(); ?>gohan旅 </span>をつけて<br>
         INSTAGRAMに投稿してくださいね！</p>
-      <p class="btn-wrap"><a href="https://www.instagram.com/explore/tags/gohan旅/?hl=ja" class="btn btn-m-blue"><span class="bold-text"><?php get_pref(); ?>GOHAN旅
-          </span>の投稿をみる</a>
+      <p class="btn-wrap-insta">
+        <a href="https://www.instagram.com/explore/tags/gohan旅/?hl=ja" class="btn btn-m-blue">
+          <span class="bold-text"><?php get_pref(); ?>GOHAN旅</span>の投稿をみる
+        </a>
       </p>
     </div>
   </div>
