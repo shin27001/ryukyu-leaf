e5b816b0f9a057082e42a83f547c4764a6693b1c
matayoshi <matayoshi@sem-cloud.com>
2020-09-11 11:28:49 +0900

    タクソノミー検索を修正

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
     <?php
+        $url = parse_url('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+        list($dummy1, $pref, $taxonomy, $param, $dummy2) = explode("/", $url['path']);
         $args = array(
           'post_type' => 'shops',
+          'orderby' => 'rand',
+          'tax_query' => array(
+                  array(
+                      'taxonomy' => $taxonomy,
+                      'terms' => array( $param ),
+                      'field' => 'slug',
+                  )
+                )
         );
         query_posts($args);
     ?>
@@ -10,4 +20,5 @@
         <?php get_template_part('tpl', 'shoplist'); ?>
       </div>
     </div>
+    <?php wp_reset_query(); ?>
 <?php get_footer(); ?>
