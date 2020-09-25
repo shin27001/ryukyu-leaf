44e7511247b2019efc67ff3310e2500e11cd2f28
matayoshi <matayoshi@sem-cloud.com>
2020-09-25 11:42:04 +0900

    メインタクソノミー取得。新着店舗

--- tpl-shoplistnew.php
+++ tpl-shoplistnew.php
@@ -44,11 +44,8 @@
                   <?php endif; ?>
                   <h3 class="l-shop__infoTitle"><?php the_title(); ?></h3>
                   <p class="l-shop__infoArea">
-                    <?php //$area = gt_get_main_term(get_the_ID(), 'area'); ?>
-                    <?php $areas = get_the_terms(get_the_ID(), 'area'); ?>
-                    <?php if ($areas) : foreach($areas as $area) : ?>
-                      <?php echo ($area->parent) ? '<i class="fas fa-map-marker-alt"></i>'.$area->name : ""; ?>
-                    <?php endforeach; endif; ?>
+                    <?php $area = gt_get_main_term(get_the_ID(), 'area'); ?>
+                    <i class="fas fa-map-marker-alt"></i> <?php echo $area->name; ?>
                   </p>
                 </div>
               </a>
