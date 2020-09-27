7d75df3a4c4f0001a123a3aa98962766aed75202
matayoshi <matayoshi@sem-cloud.com>
2020-09-27 23:20:32 +0900

    エリア複数登録。メインタクソノミー取得処理リファクタリング

--- functions.php
+++ functions.php
@@ -87,20 +87,24 @@ function get_auth() {
 // }
 function gt_get_main_term($postid, $taxonomy) {
   $term = [];
+  
+  $terms = get_the_terms($postid, $taxonomy);
+  if (empty($terms)) { return; }
+
   if (class_exists('WPSEO_Primary_Term')) {
     $wpseo_primary_term = new WPSEO_Primary_Term($taxonomy, $postid);
     $wpseo_primary_term = $wpseo_primary_term->get_primary_term();
     $term = get_term($wpseo_primary_term);
     //メインのタクソノミーが無い場合
-    if(is_wp_error($term)) {
-      $terms = get_the_terms($postid, $taxonomy);
+    if(!is_wp_error($term)) {
+      return $term; 
+    } else {
       foreach ($terms as $key => $trm) {
         if($trm->parent) {
-          $other_term = $trm;
-          break;
+          return $trm;
         }
       }
-      return (!empty($other_term)) ? $other_term : get_the_terms($postid, $taxonomy)[0]; 
+      return $terms[0]; 
     }
   }
   return $term;
